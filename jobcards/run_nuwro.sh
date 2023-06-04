0b0852fbecc51b313c252f71719a7d08203ea90b
afropapp13 <afropapp@yahoo.gr>
2023-06-04 14:52:44 -0500

    Jun 4 2023, improvements on nuwro run card due to nuisance changes

--- jobcards/run_nuwro.sh
+++ jobcards/run_nuwro.sh
@@ -6,25 +6,23 @@
 # old fashioned way
 ##nuwro -i NuWroCard_CC_Ar_uBFlux.txt -p "beam_particle = 14"  -p "beam_inputroot_flux = hEnumu_cv" -p "beam_inputroot = MCC9_FluxHist_volTPCActive.root"
 # new way, May 22 2023
-nuis gen NuWro -f  MCC9_FluxHist_volTPCActive.root,hEnumu_cv -P numu -n 100 -t Ar
+nuis gen NuWro -f  MCC9_FluxHist_volTPCActive.root,hEnumu_cv -P numu -n 10000 -t Ar -o NuWroCard_CC_Ar_uBFlux.prep.root
 
 # Prepare NuWro events for Nuisance
 # old way
 ##PrepareNuwroEvents -f eventsout.root -F MCC9_FluxHist_volTPCActive.root,hEnumu_cv -o NuWroCard_CC_Ar_uBFlux.prep.root
 # new way, May 22 203
-PrepareNuWroEvents -F MCC9_FluxHist_volTPCActive.root,hEnumu_cv,14 NuWro.numu.hEnumu_cv.MCC9_FluxHist_volTPCActive.1280.evts.root -o NuWro.prep.root
+PrepareNuWroEvents -F MCC9_FluxHist_volTPCActive.root,hEnumu_cv,14 NuWroCard_CC_Ar_uBFlux.prep.root -o NuWro.prep.root
 
 # Convert to Nuisance flat tree
 nuisflat -i NuWro:NuWro.prep.root -o samples/NuWro.flat.root
 
 # Remove unnecessary files
-#rm eventsout.root.par
-#rm eventsout.root.txt
-#rm eventsout.root
-#rm q0.txt
-#rm q2.txt
-#rm qv.txt
-#rm totals.txt
-#rm random_seed
-#rm T.txt
-#mv NuWro.prep.root samples/
+rm NuWroCard_CC_Ar_uBFlux.prep.root
+rm NuWro.*
+rm NuWroCard_CC_Ar_uBFlux.prep.root.*
+rm Ar*.txt
+rm q*.txt
+rm T.txt
+rm random_seed
+rm totals.txt
