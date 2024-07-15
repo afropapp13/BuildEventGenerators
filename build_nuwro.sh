b8c04744d7905d47d6313c8f233d9a2fb6566a57
afropapp13 <afropapp@yahoo.gr>
2024-07-15 10:49:53 -0500

    Jul 15 2024: using nuwro tagged version

--- build_nuwro.sh
+++ build_nuwro.sh
@@ -4,6 +4,7 @@ source global_vars.sh
 
 git clone https://github.com/NuWro/nuwro.git nuwro
 cd nuwro
+git checkout nuwro_21.09.02
 export PATH=$PATH:$ROOTSYS/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOTSYS/lib
 export PYTHIA6=${PYTHIA_FQ_DIR}/lib
