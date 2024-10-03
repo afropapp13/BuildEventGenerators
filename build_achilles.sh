2c75ea8410116d82f31847d089890ceb17b1a8fb
afropapp13 <afropapp@yahoo.gr>
2024-10-03 15:06:35 -0500

    Oct 3 2024: building ACHILLES after CEWG talk by Josh Isaacson using dev branch which now includes RES contributions

--- build_achilles.sh
+++ build_achilles.sh
@@ -1,31 +1,50 @@
 #!/bin/bash                                                                                                                                
 
+export TERM=screen
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" && pwd )"
+source ${BASE_DIR}/global_vars.sh
 
-# MicroBooNE                                                                                                                                                                                            
-source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
 
-# SBND                                                                                                                                                                                                 
-#source /cvmfs/sbnd.opensciencegrid.org/products/sbnd/setup_sbnd.sh                                                                                                                                      
-#DUNE                                                                                                                                                                                                   
-#source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh                                                                                                                                      
+# MicroBooNE
+if [[ "${hostgpvm}" == "uboone" ]] 
+then 
+	source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
+fi
+
+# SBND
+if [[ "${hostgpvm}" == "sbnd" ]] 
+then 
+	source /cvmfs/sbnd.opensciencegrid.org/products/sbnd/setup_sbnd.sh
+fi
+
+#DUNE
+if [[ "${hostgpvm}" == "dune" ]] 
+then 
+	source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+fi
+
+unsetup
 setup root v6_22_08b -q e20:p383b:prof
-setup lhapdf v5_9_1k -q e17:debug
-setup log4cpp v1_1_3a -q e17:debug
-setup pdfsets v5_9_1b
-setup gdb v8_1
+#setup lhapdf v5_9_1k -q e17:debug
+#setup log4cpp v1_1_3a -q e17:debug
+#setup pdfsets v5_9_1b
+#setup gdb v8_1
+unsetup git
 setup git v2_15_1
-setup cmake v3_14_3
+#unsetup cmake
+#setup cmake v3_14_3
 setup hdf5 v1_12_0b -q e20:prof 
 
-git clone https://github.com/AchillesGen/Achilles
-cd Achilles
-git checkout 6ff810ad7e3fa5daa5f94d7f5c52a93070664433
+git clone https://github.com/AchillesGen/Achilles achilles
+cd achilles
+git checkout dev
 
-mkdir build && cd build
+mkdir build
+cd build
 cmake .. -DENABLE_BSM=OFF -DUSE_ROOT=ON
 make -j4
 
 export LD_LIBRARY_PATH=/cvmfs/larsoft.opensciencegrid.org/products/gcc/v9_3_0/Linux64bit+3.10-2.17/lib64/:$LD_LIBRARY_PATH
 
 cd $BASE_DIR
+
--- jobcards/run_achilles.sh
+++ jobcards/run_achilles.sh
@@ -1,26 +1,48 @@
 #!/bin/bash                                                                                                                                
 
+export gpvm=${HOSTNAME}
+export suffix=".fnal.gov"
+export prefix="gpvm"
+export hostgpvm=${gpvm%"$suffix"}
+hostgpvm=$(printf '%s\n' "${hostgpvm//[[:digit:]]/}")
+
+hostgpvm=${hostgpvm%"$prefix"}
+
+export TERM=screen
 BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]:-${(%):-%x}}" )" && pwd )"
 
-# MicroBooNE                                                                                                                                                                                            
-source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
+# MicroBooNE
+if [[ "${hostgpvm}" == "uboone" ]] 
+then 
+	source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
+fi
 
-# SBND                                                                                                                                                                                                 
-#source /cvmfs/sbnd.opensciencegrid.org/products/sbnd/setup_sbnd.sh                                                                                                                                      
-#DUNE                                                                                                                                                                                                   
-#source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh                                                                                                                                      
+# SBND
+if [[ "${hostgpvm}" == "sbnd" ]] 
+then 
+	source /cvmfs/sbnd.opensciencegrid.org/products/sbnd/setup_sbnd.sh
+fi
+
+#DUNE
+if [[ "${hostgpvm}" == "dune" ]] 
+then 
+	source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
+fi
+
+unsetup
 setup root v6_22_08b -q e20:p383b:prof
-setup lhapdf v5_9_1k -q e17:debug
-setup log4cpp v1_1_3a -q e17:debug
-setup pdfsets v5_9_1b
-setup gdb v8_1
+#setup lhapdf v5_9_1k -q e17:debug
+#setup log4cpp v1_1_3a -q e17:debug
+#setup pdfsets v5_9_1b
+#setup gdb v8_1
+unsetup git
 setup git v2_15_1
-setup cmake v3_14_3
+#unsetup cmake
+#setup cmake v3_14_3
 setup hdf5 v1_12_0b -q e20:prof 
 
-cd ../Achilles/build
+cd ../achilles/build
 
 export LD_LIBRARY_PATH=/cvmfs/larsoft.opensciencegrid.org/products/gcc/v9_3_0/Linux64bit+3.10-2.17/lib64/:$LD_LIBRARY_PATH
 
-./bin/achilles run_uboone.yml 
-
+./bin/achilles ../../jobcards/run_uboone.yml 
