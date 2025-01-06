c7c26572ccc13b7132341cfc349fde4ab9590870
afropapp13 <afropapp@yahoo.gr>
2025-01-06 10:58:37 -0600

    Jan 6 2025: migrating to GiBUU 2025

--- build_gibuu.sh
+++ build_gibuu.sh
@@ -3,13 +3,10 @@
 source global_vars.sh
 
 mkdir GiBUU; cd GiBUU
-wget --content-disposition https://gibuu.hepforge.org/downloads?f=release2023.tar.gz;tar -xzvf release2023.tar.gz
-wget --content-disposition https://gibuu.hepforge.org/downloads?f=buuinput2023.tar.gz; tar -xzvf buuinput2023.tar.gz
-wget --content-disposition https://gibuu.hepforge.org/downloads?f=libraries2023_RootTuple.tar.gz; tar -xzvf libraries2023_RootTuple.tar.gz
+wget --content-disposition https://gibuu.hepforge.org/downloads?f=release2025.tar.gz;tar -xzvf release2025.tar.gz
+wget --content-disposition https://gibuu.hepforge.org/downloads?f=buuinput2025.tar.gz; tar -xzvf buuinput2025.tar.gz
+wget --content-disposition https://gibuu.hepforge.org/downloads?f=libraries2025_RootTuple.tar.gz; tar -xzvf libraries2025_RootTuple.tar.gz
 sed -i 's/-std=c++11/-std=c++17/g' libraries/RootTuple/RootTuple-master/src/CMakeLists.txt
-#git clone https://github.com/afropapp13/GitGiBUU.git
-#mv GitGiBUU/EventOutputFile.f90 release/code/inputOutput/EventOutputFile.f90
-#rm -rf GitGiBUU
 
 mkdir build; cd build
 cmake -DCMAKE_INSTALL_PREFIX=./ ../libraries/RootTuple/RootTuple-master/
@@ -20,12 +17,6 @@ cp lib/libRootTuple.a ../release/objects/LIB/lib/libRootTuple.100.a
 
 cd ../release
 
-# After discussion with Andy Mastbaum & C Wilkinson
-# For fields x, y, z, you need to go back to your GiBUU build and, instead of make buildRootTuple, 
-# do make buildRootTuple_POS  (version of the ntupler with the vtx location w/in the nucleus)
-# Then re-run make withROOT
-
 make buildRootTuple_POS
-#make buildRootTuple
 
 make withROOT=1
