52077702d4c0e3fc2fac1bbdd63f36241fbaac30
afropapp13 <afropapp@yahoo.gr>
2025-02-25 15:46:54 -0600

    Feb 21 2025: updating achilles instructions

--- jobcards/run_achilles.sh
+++ jobcards/run_achilles.sh
@@ -2,15 +2,12 @@
 
 cd ../achilles/build
 
-./bin/achilles ../../jobcards/run_uboone.yml
+./bin/achilles ../../jobcards/run_argon.yml
 
 cd -
-mv ../achilles/build/achilles.hepmc ./
+mv ../achilles/build/achilles_argon.hepmc ./
 
-## this line is not working
-#nuisflat -i NuHepMC:achilles.hepmc -o achilles.flat.root  
-
-cp achilles.hepmc ../neut_container/interactive/
-cd ../neut_container/interactive
-/cvmfs/oasis.opensciencegrid.org/mis/apptainer/current/bin/apptainer exec nuisance_nuint2024.sif /bin/bash nuisflat -i NuHepMC:achilles.hepmc -o achilles.flat.root
-
+nuisflat -i NuHepMC:achilles_argon.hepmc -o achilles.flat.root 
+mv achilles_argon.hepmc samples/
+mv achilles.flat.root samples/
+ 
