e362fd265a65045ccfbfb3d2d47dde1797abcf14
afropapp13 <afropapp@yahoo.gr>
2025-12-03 11:55:48 -0600

    Dec 3 2025: nuisance and nuwro fixes to accommodate nuwro 25.11

--- build_nuwro.sh
+++ build_nuwro.sh
@@ -4,7 +4,7 @@ source global_vars.sh
 
 git clone https://github.com/NuWro/nuwro.git nuwro
 cd nuwro
-git checkout tags/NuWro_25_03_1
+git checkout tags/nuwro_25.11
 export PATH=$PATH:$ROOTSYS/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOTSYS/lib
 export PYTHIA6=${PYTHIA_FQ_DIR}/lib
